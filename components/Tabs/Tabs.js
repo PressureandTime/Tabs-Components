c62c486d40fae982466b66649505bd473cfa8aa1
Petar Scepanovic <hiborija@gmail.com>
2019-05-23 17:42:26 +0200

    some changes

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,26 +1,25 @@
-
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     this.element = element;
-    
+
     // Get the custom data attribute on the Link
 
     const itemElement = document.querySelector(`.tabs-item[data-tab='${element.dataset.tab}']`);
-    
+
     //  this.data = document.querySelector(``)
 
-    
+
     // Using the custom data attribute get the associated Item element
     this.itemElement = new TabItem(itemElement);
- 
+
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
+
     element.addEventListener('click', () => this.select());
     // Add a click event listener on this instance, calling the select method on click
-    
-  };
+
+  }
 
   select() {
     // Get all of the elements with the tabs-link class
@@ -32,9 +31,9 @@ class TabLink {
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
     console.log(this.element);
-    
+
     // Call the select method on the item associated with this link
-     this.itemElement.select();
+    this.itemElement.select();
   }
 }
 
@@ -49,15 +48,15 @@ class TabItem {
     const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-    
-    items.forEach(item => item.classList.remove('tabs-item-selected'))
+
+    items.forEach(item => item.classList.remove('tabs-item-selected'));
 
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-item-selected');
   }
 }
 
-/* START HERE: 
+/* START HERE:
 
 - Select all classes named ".tabs-link" and assign that value to the links variable
 
@@ -67,6 +66,6 @@ class TabItem {
 
 */
 
-let links = document.querySelectorAll('.tabs-link');
+const links = document.querySelectorAll('.tabs-link');
 
 links.forEach(tabLink => new TabLink(tabLink));
